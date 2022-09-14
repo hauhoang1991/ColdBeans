b55b995bc546823600c5820b0ce27d862ff1c4b3
Hau Hoang <hauhoangduc@gmail.com>
2022-09-14 10:51:26 -0700

    Initial Commit

---
+++ ColdBeans/ColdBeansApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColdBeansApp.swift
+//  ColdBeans
+//
+//  Created by Hau Hoang on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ColdBeansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
